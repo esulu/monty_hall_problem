3ac8420ee538390f9287fd69d94f3cb2276601ee
Eren Sulutas <erensulutas@hotmail.com>
2019-07-31 20:10:30 -0400

    initial commit

---
+++ monty.py
@@ -0,0 +1,23 @@
+'''
+Monty Hall Problem
+
+Background info:
+Based on the famous gameshow where contestants have a choice to pick between 3 doors
+Two doors contain goats, and the contestant loses the game
+One door contains money, which is what the contestant desires
+
+Rules:
+- The contestant picks one door at random, not knowing what is behind it
+- The host opens one of the other doors, and reveals a goat regardless of what lies behind the contestants' door
+- The host gives the contestant a choice, whether to switch doors or not
+- The contestant makes their choice and their final choice is revealed
+
+Theory:
+In the stage where the host opens a door revealing a goat, it might be thought that there is a 50/50 chance if the contestant wins the money or not
+However, this is incorrect. 
+
+
+
+Program made by Eren Sulutas
+
+'''
